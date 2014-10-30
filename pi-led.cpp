4e304ca27fb25a2003b04e6b952e6b9979315b27
Vidar S. Ramdal <vidar.ramdal@webstep.no>
2014-10-30 01:31:12 +0100

    As per https://github.com/robotastic/pi-led/issues/1

--- pi-led.cpp
+++ pi-led.cpp
@@ -505,7 +505,7 @@ Handle<Value> PiLed::WriteMessage(const Arguments& args) {
     // Schedule our work request with libuv. Here you can specify the functions
     // that should be executed in the threadpool and back in the main thread
     // after the threadpool function completed.
-    int status = uv_queue_work(uv_default_loop(), req, AsyncWork, AsyncAfter);
+    int status = uv_queue_work(uv_default_loop(), req, AsyncWork, (uv_after_work_cb)AsyncAfter);
     assert(status == 0);
 
     return Undefined();
